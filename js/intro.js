645eb131ad51e5126a9d4d828789fc2c3f7a0a5e
Swatisavaliya1201 <swatisavaliya1201@gmail.com>
2023-09-01 10:06:27 +0530

    intro

---
+++ js/intro.js
@@ -0,0 +1,11 @@
+// debugg
+console.log("Hello World!");
+
+document.write("<h1> Hello Javascript </h1>")
+
+alert("OK");
+
+let x = parseInt(prompt("Please Enter Value x:",0));
+console.log(x);
+
+
